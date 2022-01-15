808e1ed164ff2e4c159983171c5a656138e9fe45
Stanley <stanleygarbo@gmail.com>
2022-01-15 16:24:35 +0800

    added staking data

--- contexts/walletContext.tsx
+++ contexts/walletContext.tsx
@@ -1,6 +1,7 @@
 import React, { useState, createContext, useContext, useEffect } from "react";
 import { IWalletContext } from "../interfaces/contexts/IWalletContext";
-import ContractABI from "../contracts/ChumbiABI.json";
+import ChumbiContractABI from "../contracts/ChumbiABI.json";
+import CHMBStakingContractABI from "../contracts/StakingABI.json";
 import Web3 from "web3";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import { AbiItem } from "web3-utils";
@@ -20,13 +21,34 @@ declare global {
   }
 }
 
-const contractAddress = "0x5492ef6aeeba1a3896357359ef039a8b11621b45";
-const abi = ContractABI;
+const ChumbiContractAddress = "0x5492ef6aeeba1a3896357359ef039a8b11621b45";
+const ChumbiABI = ChumbiContractABI;
+const CHMBStaking_90days_ContractAddress =
+  "0xaf55804d5ddb764fb444e00d99fec9e52883ee17";
+const CHMBStaking_180days_ContractAddress =
+  "0x20112dFceE4C22057aB6751AE1ac2DE0109C468c";
+const CHMBStaking_365days_ContractAddress =
+  "0x1568F0E8ff865bE61D2D735AD1Ba07f199A4590a";
+
+const CHMBStakingABI = CHMBStakingContractABI;
+
+const BSCProvider = new Web3.providers.HttpProvider(
+  "https://bsc-dataseed.binance.org/"
+);
+const BSCWeb3 = new Web3(BSCProvider);
+
+const polygonProvider = new Web3.providers.HttpProvider(
+  "https://polygon-rpc.com/"
+);
+const polygonWeb3 = new Web3(polygonProvider);
 
 const useWalletContext = () => {
   const [current, setCurrent] = useState<string>();
-  const [web3, setWeb3] = useState<Web3>();
-  const [chumbiContract, setChumbiContract] = useState<any>();
+
+  const ChumbiContract = new polygonWeb3.eth.Contract(
+    ChumbiABI.ContractABI as AbiItem[],
+    ChumbiContractAddress
+  );
 
   const connectWalletHandler = async () => {
     const { ethereum } = window;
@@ -35,31 +57,79 @@ const useWalletContext = () => {
       alert("please install metamask"!);
     }
 
-    if (web3)
+    try {
+      if (!current) {
+        const accounts = await ethereum.request<string[]>({
+          method: "eth_requestAccounts",
+        });
+
+        if (accounts) setCurrent(accounts[0]);
+
+        localStorage.setItem("WalletConnected", "true");
+      }
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Error) alert(err.message);
+    }
+  };
+
+  const stakingData = async ({
+    address,
+    duration,
+  }: {
+    address: string;
+    duration: 90 | 180 | 365;
+  }) => {
+    const baseUnitizeAndToFixedIfNecessary = (rewards: number) =>
+      +parseFloat(Web3.utils.fromWei(rewards.toString())).toFixed(2);
+
+    const baseUnitize = (rewards: number) =>
+      Number(Web3.utils.fromWei(rewards.toString())).toFixed(0);
+
+    if (BSCWeb3)
       try {
-        if (!current) {
-          if ((await web3.eth.net.getId()) !== 137) {
-            throw Error("Connect to polygon");
-          }
+        const CHMBStakingContract = new BSCWeb3.eth.Contract(
+          CHMBStakingABI.a as AbiItem[],
+          duration === 90
+            ? CHMBStaking_90days_ContractAddress
+            : duration === 180
+            ? CHMBStaking_180days_ContractAddress
+            : CHMBStaking_365days_ContractAddress
+        );
 
-          const accounts = await ethereum.request<string[]>({
-            method: "eth_requestAccounts",
-          });
+        const rewards = await CHMBStakingContract.methods
+          .earned(address)
+          .call();
+        const staked = await CHMBStakingContract.methods
+          .balanceOf(address)
+          .call();
+        const periodFinish = await CHMBStakingContract.methods
+          .periodFinish()
+          .call();
+        const stakingTill = await CHMBStakingContract.methods
+          .stakingTill()
+          .call();
+        const stakingCap = await CHMBStakingContract.methods
+          .stakingCap()
+          .call();
 
-          if (accounts) setCurrent(accounts[0]);
-
-          localStorage.setItem("WalletConnected", "true");
-        }
+        return {
+          staked: baseUnitizeAndToFixedIfNecessary(staked),
+          rewards: baseUnitizeAndToFixedIfNecessary(rewards),
+          periodFinish,
+          stakingTill,
+          stakingCap: baseUnitize(stakingCap),
+        };
       } catch (err) {
-        console.error(err);
+        return {};
       }
   };
 
   const tokenURI = async (tokenID: number) => {
-    if (web3) {
+    if (polygonWeb3) {
       try {
         // const contract = new web3.eth.Contract(abi.ContractABI, contractAddress);
-        const tokenUri = await chumbiContract.methods.tokenURI(tokenID).call();
+        const tokenUri = await ChumbiContract.methods.tokenURI(tokenID).call();
         return tokenUri;
       } catch (err) {
         return 0;
@@ -68,9 +138,9 @@ const useWalletContext = () => {
   };
 
   const totalChumbi = async (address: string) => {
-    if (web3) {
+    if (polygonWeb3) {
       // const contract = ;
-      const balance = await chumbiContract.methods.balanceOf(address).call();
+      const balance = await ChumbiContract.methods.balanceOf(address).call();
       return balance;
     }
     return 0;
@@ -84,17 +154,6 @@ const useWalletContext = () => {
     const { ethereum } = window;
 
     async function asynInit() {
-      // connect to polygon rpc
-
-      const provider = new Web3.providers.HttpProvider(
-        "https://polygon-rpc.com/"
-      );
-      const web3 = new Web3(provider);
-      setWeb3(web3);
-      setChumbiContract(
-        new web3.eth.Contract(abi.ContractABI as AbiItem[], contractAddress)
-      );
-
       // connect automatically if the user has already connected wallet before
       const hasConnectedWalletBefore = localStorage.getItem("WalletConnected");
       if (hasConnectedWalletBefore) {
@@ -115,6 +174,7 @@ const useWalletContext = () => {
     connectWallet: connectWalletHandler,
     tokenURI,
     totalChumbi,
+    stakingData,
   };
 };
 
--- interfaces/contexts/IWalletContext.ts
+++ interfaces/contexts/IWalletContext.ts
@@ -5,4 +5,27 @@ export interface IWalletContext {
   connectWallet?: () => void;
   tokenURI: (tokenID: number) => Promise<any> | void;
   totalChumbi: (address: string) => Promise<number> | void;
+  stakingData?: ({
+    address,
+    duration,
+  }: {
+    address: string;
+    duration: 90 | 180 | 365;
+  }) => Promise<
+    | {
+        staked: number;
+        rewards: number;
+        periodFinish: any;
+        stakingTill: any;
+        stakingCap: string;
+      }
+    | {
+        staked?: undefined;
+        rewards?: undefined;
+        periodFinish?: undefined;
+        stakingTill?: undefined;
+        stakingCap?: undefined;
+      }
+    | undefined
+  >;
 }
