76e0605dfa13027d6a3619b58c6f97dec5dfabcb
Stanley <stanleygarbo@gmail.com>
2022-01-20 16:59:19 +0800

    removed unused import

--- pages/finder/[id].tsx
+++ pages/finder/[id].tsx
@@ -1,5 +1,4 @@
 import { NextPage } from "next";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
