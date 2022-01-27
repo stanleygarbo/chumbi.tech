bc46e927899cc7516ead24515790192ca35738ad
Stanley <stanleygarbo@gmail.com>
2022-01-27 17:43:49 +0800

    temporarily changed url

--- api/FetchChumbiOfAddress.ts
+++ api/FetchChumbiOfAddress.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const FetchChumbiOfAddress = (address: string) => {
-  return axios
-    .get(`https://api-gateway.chumbi.tech/${address}/chumbi`)
-    .then((res) => res.data);
+  return (
+    axios
+      // .get(`https://api-gateway.chumbi.tech/${address}/chumbi`)
+      .get(`http://localhost:5000/${address}/chumbi`)
+      .then((res) => res.data)
+  );
 };
 
 export default FetchChumbiOfAddress;
