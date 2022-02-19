83618d064d42a0eaa0889d44f436ac6f4db6c56a
Stanley <stanleygarbo@gmail.com>
2022-02-19 14:25:21 +0800

    added analytics tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,6 +53,23 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-RBTBHTQZNW"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                  dataLayer.push(arguments);
+                }
+                gtag("js", new Date());
+
+                gtag("config", "G-RBTBHTQZNW");  
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
