6cec543c7f1f1c53a8d60313286914d588f6165b
Stanley <stanleygarbo@gmail.com>
2022-03-11 17:26:20 +0800

    added set current on mount

--- pages/tracker/[address]/inventory.tsx
+++ pages/tracker/[address]/inventory.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { useQuery } from "react-query";
 import styled, { css } from "styled-components";
 import FetchFoundersCollection from "../../../api/FetchFoundersCollection";
 import ErrorMessage from "../../../components/ErrorMessage";
 import Inventory from "../../../components/tracker/Inventory";
 import { useTheme } from "../../../contexts/themeContext";
+import { useWallet } from "../../../contexts/walletContext";
 import { IFoundersCollectionNFT } from "../../../interfaces/api/IFetchFoundersCollection";
 import { IColors } from "../../../interfaces/IColors";
 import { isWalletAddressValid } from "../../../util/isWalletAddressValid";
@@ -14,6 +16,7 @@ const InventoryPage: NextPage = () => {
   const router = useRouter();
   const { address } = router.query;
   const { colors } = useTheme();
+  const { setCurrent } = useWallet();
 
   const { data, isFetching } = useQuery<any, unknown, IFoundersCollectionNFT[]>(
     "FoundersCollection",
@@ -24,7 +27,10 @@ const InventoryPage: NextPage = () => {
     }
   );
 
-  console.log(data);
+  useEffect(() => {
+    if (address && isWalletAddressValid(address))
+      setCurrent(address.toString());
+  }, [address]);
 
   if (address && isWalletAddressValid(address))
     return (
